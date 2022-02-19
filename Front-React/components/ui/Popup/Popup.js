11150dcee263e7bff44a9381c549e2c0348ca7b6
MellakSoufiane <mellak.soufiane12@gmail.com>
2022-02-19 22:45:09 +0100

    select file info

--- Front-React/components/ui/Popup/Popup.js
+++ Front-React/components/ui/Popup/Popup.js
@@ -3,13 +3,34 @@ import { DescBox, LabelBox, Popupbox, BoxImg, BoxSelect } from './BoxPop';
 import { TitlePopup ,Exit,Box,ButtonSubmit} from './BoxPop';
 import { FiVideo,FiX } from "react-icons/fi";
 import SelectButton from '../SelectButton/SelectButton';
-
+import { useState } from 'react';
+import axios from 'axios';
+import { toast} from 'react-toastify';
+import { Preview } from '.';
 
 const Popup = props => {
-
+  const [files, setFiles] = useState([]);
     const onInputChange = (e) => {
       console.log(e.target.value)
     }
+    const onSubmit = (e) => {
+      e.preventDefault();
+
+      const data = new FormData();
+
+      for(let i = 0; i < files.length; i++) {
+          data.append('file', files[i]);
+      }
+
+      axios.post('//localhost:8000/upload', data)
+          .then((response) => {
+              toast.success('Upload Success');
+              onSuccess(response.data)
+          })
+          .catch((e) => {
+              toast.error('Upload Error')
+          })
+  };
 
 
   return (
@@ -21,6 +42,7 @@ const Popup = props => {
         <Exit className="close-icon" onClick={props.handleClose}><FiX/></Exit>
         {props.content}
         <Box>
+          <form onSubmit={onSubmit}>
             <BoxImg><FiVideo size={70}/></BoxImg>
             
             <LabelBox>Drag and Drop video files to upload</LabelBox>
@@ -33,7 +55,9 @@ const Popup = props => {
            
             </SelectButton></BoxSelect>
             <ButtonSubmit>Submit</ButtonSubmit>
+            </form>
         </Box>
+        <Preview files={files}/>
     </Popupbox>
     
   );
