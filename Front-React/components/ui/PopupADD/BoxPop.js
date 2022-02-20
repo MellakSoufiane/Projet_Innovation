8f4154429fa05fe8b7146070d8e8ed1660a03511
MellakSoufiane <mellak.soufiane12@gmail.com>
2022-02-20 01:01:25 +0100

    Upload Bar + Placeholder ImageVideo

--- Front-React/components/ui/PopupADD/BoxPop.js
+++ Front-React/components/ui/PopupADD/BoxPop.js
@@ -10,9 +10,10 @@ top: 100.9px;
 background-color: red;
 z-index:1;
 
-
 background: #FFFFFF;
-border-radius: 16px;
+box-shadow: 0px 8px 8px #EDEFF2;
+border-radius: 8px;
+
 `
 export const TitlePopup = styled.h1`
 position: absolute;
@@ -34,8 +35,8 @@ color: #292B2E;
 `
 export const Exit = styled.p`
 position: absolute;
-top: 32px;
-left: 850px;
+top: 10px;
+left: 870px;
 width: 12px;
 height: 12px;
 cursor: pointer;
@@ -86,6 +87,37 @@ top: 50px;
 
 
 `
+export const LabelDesc = styled.p`
+/* Title (required) */
+
+
+position: absolute;
+width: 149px;
+height: 20px;
+left: 50px;
+top: 180px;
+
+font-family: Montserrat;
+font-style: normal;
+font-weight: 500;
+font-size: 15px;
+line-height: 150%;
+
+
+display: flex;
+align-items: center;
+
+color: #44484E;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 0;
+flex-grow: 0;
+margin: 8px 0px;
+`
+
 
 export const Input = styled.input`
 position: absolute;
@@ -93,8 +125,49 @@ width: 464px;
 height: 40px;
 left: 52px;
 top: 121px;
-border: 1px F8F9FB;
+border: 3px solid #555;
 border-radius: 3px;
+&::placeholder {
+    
+    color: var(--input-placeholder-color);
+  }
 
+
+`
+export const Desc = styled.textarea`
+position: absolute;
+text-align: justify;
+width: 464px;
+height: 100px;
+  
+ 
+left: 52px;
+top: 220px;
+border: 3px solid #555;
+border-radius: 3px;
+&::placeholder {
+    text-align: justify;
+    color: var(--input-placeholder-color);
+  }
+
+
+`
+export const Img = styled.div`
+/* social link */
+position: absolute;
+width: 300px;
+height: 253px;
+left: 24px;
+top: 24px;
+
+
+
+
+
+/* Light/White */
+
+background: #FFFFFF;
+box-shadow: 0px 8px 8px #EDEFF2;
+border-radius: 8px;
 
 `
--- Front-React/components/ui/PopupADD/Popup.js
+++ Front-React/components/ui/PopupADD/Popup.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {  LabelBody, Popupbox} from './BoxPop';
+import {  Desc, LabelBody,LabelDesc, Popupbox,Img} from './BoxPop';
 import { TitlePopup ,Exit,Box,Input} from './BoxPop';
 import { FiX } from "react-icons/fi";
-
+import { CustomPlaceholder } from 'react-placeholder-image';
+import { CgLoadbar } from "react-icons/cg";
 
 const Popupadd = props => {
  
@@ -18,10 +19,14 @@ const Popupadd = props => {
         {props.content}
         
         
-        <Box></Box>
+        <Box><Img><CustomPlaceholder />
+        <CgLoadbar size={40}/>
+
+        </Img></Box>
         <LabelBody>Title (required)</LabelBody>
-        <Input></Input>
-       
+        <Input placeholder='Enter Title'></Input>
+        <LabelDesc>Description</LabelDesc>
+        <Desc placeholder='Write Long Text here'></Desc>
     </Popupbox>
     
   );
